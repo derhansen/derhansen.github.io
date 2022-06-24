f7906a22f3f86f790f291e5805f604128b0207ce
Torben Hansen <derhansen@gmail.com>
2022-06-24 14:23:43 +0200

    Updated layout

--- assets/js/derhansen.js
+++ assets/js/derhansen.js
@@ -9,3 +9,8 @@ function toggleNightMode () {
     localStorage.setItem('theme', '')
   }
 }
+
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  document.documentElement.setAttribute('data-theme', 'dark');
+  document.getElementById('mode-switcher').classList.add('active');
+}
